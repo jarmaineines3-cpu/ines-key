baf5c14a59741895fbd84c0099ba7bdb1566578f
jarmaineines3-cpu <jarmaineines3@gmail.com>
2026-07-24 13:24:28 +0800

    ADD migration

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,7 @@ php artisan storage:link || true
 php artisan config:clear || true
 php artisan cache:clear || true
 php artisan package:discover --ansi || true
+php artisan migrate --force || true
 
 php-fpm -D
 exec nginx -g "daemon off;"
